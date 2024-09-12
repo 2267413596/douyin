625f7f95d06894aaba9a36a07e9dcbdc23f11834
2267413596 <2267413596@qq.com>
2024-09-12 23:49:39 +0800

    mybatis config

--- douyin-api/src/main/java/com/imooc/Application.java
+++ douyin-api/src/main/java/com/imooc/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(exclude={MongoAutoConfiguration.class})
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
