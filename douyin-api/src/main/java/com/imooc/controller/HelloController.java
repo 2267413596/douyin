fe63b5bb93235fbc413d725b23e64bf897ca9ac6
2267413596 <2267413596@qq.com>
2024-09-13 22:13:25 +0800

    knife 4 j

--- douyin-api/src/main/java/com/imooc/controller/HelloController.java
+++ douyin-api/src/main/java/com/imooc/controller/HelloController.java
@@ -3,14 +3,18 @@ package com.imooc.controller;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.grace.result.ResponseStatusEnum;
 import com.imooc.model.Stu;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Hello测试接口")
 @Slf4j
 public class HelloController {
     @GetMapping("hello")
+    @ApiOperation(value = "返回测试")
     public Object hello(){
         Stu stu = new Stu("imooc", 18);
         log.debug(stu.toString());
